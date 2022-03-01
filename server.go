4659f327bd6b7f174350b1ef1a2f037a506cabbb
sagor <sagor.eee.cuet@gmail.com>
2022-03-01 18:09:02 +0600

    entity, service, controller in go-gin

--- server.go
+++ server.go
@@ -1,6 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"example.com/sagor/go-web-gin/controller"
+	"example.com/sagor/go-web-gin/service"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
 
 func main() {
 	server := gin.Default()
@@ -9,6 +18,14 @@ func main() {
 		ctx.JSON(200, gin.H{
 			"message": "OK!!",
 		})
+	})
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
 	})
 
 	server.Run(":8080")
