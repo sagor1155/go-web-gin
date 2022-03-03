84918389902f88c9bbe1d6459cfc35dcd8d16f35
sagor1155 <sagor.eee.cuet@gmail.com>
2022-03-04 02:39:03 +0600

    added middlewares: basic auth, logger

--- server.go
+++ server.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"example.com/sagor/go-web-gin/controller"
+	"example.com/sagor/go-web-gin/middlewares"
 	"example.com/sagor/go-web-gin/service"
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -11,8 +16,18 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout) // write log output into both file and stdout
+}
+
 func main() {
-	server := gin.Default()
+	setupLogOutput()
+
+	server := gin.New()
+
+	// add middleware
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/test", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
